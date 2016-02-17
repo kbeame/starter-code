bbfe60dec77555ca6acd3d0cd0fc50e26f702619
Katherine Beame <katherine.beame@gmail.com>
2016-02-17 15:38:17 -0800

    changed ajax into a get

--- scripts/repo.js
+++ scripts/repo.js
@@ -5,16 +5,11 @@
 
   // TODO: Refactor this ajax call into a get request to the proxy end point provided by server.js.
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: 'https://api/github.com/users/kbeame/repos' +
+    $.get('https://api/github.com/users/kbeame/repos' +
             '?per_page=100' +
-            '&sort=updated',
-      type: 'GET',
-      headers: { 'Authorization': 'token ' + GITHUB_TOKEN },
-      success: function(data, message, xhr) {
-        repos.all = data;
-      }
-    }).done(callback);
+            '&sort=updated')
+    .done(function(data, message, xhr) {
+      repos.all = data;
   };
 
   repos.with = function(attr) {
