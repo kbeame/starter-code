6d7962c552b796cb8f3ff01dd4f50f88ab5d8b6c
Katherine Beame <katherine.beame@gmail.com>
2016-02-17 15:42:16 -0800

    fixed url bug

--- scripts/repo.js
+++ scripts/repo.js
@@ -5,7 +5,7 @@
 
   // TODO: Refactor this ajax call into a get request to the proxy end point provided by server.js.
   repos.requestRepos = function(callback) {
-    $.get('https://api/github.com/users/kbeame/repos' +
+    $.get('https://api.github.com/users/kbeame/repos' +
             '?per_page=100' +
             '&sort=updated')
     .done(function(data, message, xhr) {
